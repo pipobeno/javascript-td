05c4d89d3b84fdd928dd75db9ce15255fa709364
pipobeno <noura.khadraoui13003@gmail.com>
2024-10-16 10:18:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// Projet 1 Javascript : le Date parfait
+
+// let question = prompt("Salut, je m'appelle Alex enchanté ! (a): Salut, enchanté Alex! Comment vas-tu ? (b): Tu veux quoi toi ?!");
+
+// if (question === "a") {
+//     console.log("Très bien merci, tu es bien aimable");
+//     question = prompt("(c): Et si on sortait ensemble ? (d): J-je... *s'enfuit en courant*");
+    
+//     if (question === "c") {
+//         console.log("Oui sortons ensemble !");
+
+//         question = prompt("(k)Par contre il va falloir que tu perde du poids (l)Tu est parfait tel que tu es")
+
+//         if (question=== "k") {
+//             console.log("c'est finis entre nous débile !");
+//             console.log("Fin: Vous avez brisé le coeur d'Alex espèce de grossophobe !");
+//         } else if (question=== "l") {
+//             console.log("Merci tu est très bien aussi...*rougis*");
+//             console.log("Fin : Vous finissez votre vie avec Alex. Toutes mes félicitations !");
+//         }
+
+
+//     } else if (question === "d") {
+//         console.log("Vraiment étrange cette réaction...");
+//         console.log("Fin 2: Vous finissez célibataire.");
+//     }
+    
+// } else if (question === "b") {
+//     console.log("Va te faire foutre");
+//     question = prompt("(e): Viens te battre ! (f): Je m'excuse...");
+
+//     if (question === "e") {
+//         console.log("Tu vas regretter ton choix !");
+//         console.log("*Vous vous battez avec Alex.*");
+//         question = prompt("(g)Vous frappez en premier (h)Vous attendez de voir ce que Alex va faire")
+
+//         if (question=== "g") {
+//             console.log("*Vous mettez ko Alex en un coup* Souplesse Blyat!");
+//             console.log("Fin caché: Vous êtes la relève de Rocky Balboa!");
+//         } else if (question === "h") {
+//             console.log("*Alex vous met une droite et une vous assène un Juji Gatame.*");
+//             console.log("Fin caché: Vous êtes nul à la bagarre allez prendre des cours de self défense*");
+//         }
+
+
+//     } else if (question === "f") {
+//         console.log("Bon, tant que tu t'excuses...Tu est vraiment une victime");
+//         console.log("Vous avez évité le conflit.");
+//         question = prompt("(i)Par contre parle bien (j)*je m'enfuis en courant*")
+
+//         if (question=== "i") {
+//             console.log("Oh...désolé...j'y vais");
+//             console.log("Fin alternative: Vous avez victimisé Alex vous êtiez sensé le/la pecho...");
+//         }else if (question=== "j") {
+//             console.log("Casse toi débile *Alex vous lance sa chaussure*");
+//             console.log("Fin 2: Vous finissez célibataire.");
+//         }
+//     }
+// }
+
